66281584fcf67c642ee3c77aece54acaa19c0b18
nmov22 <nmo.velasco@gmail.com>
2020-10-16 16:00:15 +0800

    - added more stun servers

--- public/js/video.js
+++ public/js/video.js
@@ -3,7 +3,25 @@ const peerConnections = {}
 const config = {
   iceServers: [
     {
-      urls: ['stun:stun.l.google.com:19302']
+      urls: ['stun.l.google.com:19302',
+        'stun1.l.google.com:19302',
+        'stun2.l.google.com:19302',
+        'stun3.l.google.com:19302',
+        'stun4.l.google.com:19302',
+        'stun01.sipphone.com',
+        'stun.ekiga.net',
+        'stun.fwdnet.net',
+        'stun.ideasip.com',
+        'stun.iptel.org',
+        'stun.rixtelecom.se',
+        'stun.schlund.de',
+        'stunserver.org',
+        'stun.softjoys.com',
+        'stun.voiparound.com',
+        'stun.voipbuster.com',
+        'stun.voipstunt.com',
+        'stun.voxgratia.org',
+        'stun.xten.com']
     }
   ]
 }
@@ -22,7 +40,7 @@ socket.on('roomData', ({ room, users}) => {
     console.log('Call Broadcaster')
     if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
       var constraints = {
-          audio: true,
+          // audio: true,
           video: { facingMode: 'user' }
       }
       
